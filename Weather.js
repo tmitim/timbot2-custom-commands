4f781d3d2cbdeed950473680b035e7ffc9837cff
tmitim <igetemail@gmail.com>
2017-02-06 23:35:14 -0800

    Add: 'weather in' location

--- Weather.js
+++ Weather.js
@@ -9,7 +9,7 @@ var BotListener_1 = require("timbot2/lib/BotListener");
 var Weather = (function (_super) {
     __extends(Weather, _super);
     function Weather() {
-        var _this = _super.apply(this, arguments) || this;
+        var _this = _super !== null && _super.apply(this, arguments) || this;
         _this.name = "weather";
         _this.desc = "Shows weather information";
         _this.hidden = false;
@@ -20,13 +20,19 @@ var Weather = (function (_super) {
         var weatherCommand = this;
         weatherCommand.controller.hears('weather', weatherCommand.channels, function (bot, message) {
             var location = process.env.WEATHER_LOCATION || 'Los Angeles, CA';
+            var findLocation = message.text.split(" ");
+            if (message.text.indexOf('weather in') != -1) {
+                location = message.text.substring(message.text.indexOf('weather in') + 10, message.text.length);
+            }
             weather.find({ search: location, degreeType: 'F' }, function (err, result) {
                 if (err) {
                     console.log(err);
                 }
                 else {
-                    bot.reply(message, "Feels like " + result[0].current.feelslike + result[0].location.degreetype + " here in " +
-                        result[0].location.name + ". (" + result[0].forecast[0].high + "/" + result[0].forecast[0].low + ")");
+                    var botMessage = "Feels like " + result[0].current.feelslike + result[0].location.degreetype + " here in " +
+                        result[0].location.name + ". (" + result[0].forecast[0].high + "/" + result[0].forecast[0].low + ")";
+                    bot.reply(message, botMessage);
+                    console.log(new Date().toUTCString() + ": " + botMessage);
                     for (var day = 1; day < result[0].forecast.length; day++) {
                         if (result[0].forecast[day].precip >= 50) {
                             (day === 1) ?
--- Weather.ts
+++ Weather.ts
@@ -12,12 +12,20 @@ export class Weather extends BotListener {
     var weatherCommand = this;
     weatherCommand.controller.hears('weather', weatherCommand.channels, function(bot,message) {
       var location = process.env.WEATHER_LOCATION || 'Los Angeles, CA';
+      var findLocation = message.text.split(" ");
+      if (message.text.indexOf('weather in') != -1) {
+        location = message.text.substring(message.text.indexOf('weather in') + 10, message.text.length);
+      }
+
       weather.find({search: location, degreeType: 'F'}, function(err, result) {
         if(err) {
           console.log(err);
         } else {
-          bot.reply(message, "Feels like " + result[0].current.feelslike + result[0].location.degreetype + " here in " +
-            result[0].location.name + ". (" + result[0].forecast[0].high + "/" + result[0].forecast[0].low + ")");
+          var botMessage = "Feels like " + result[0].current.feelslike + result[0].location.degreetype + " here in " +
+            result[0].location.name + ". (" + result[0].forecast[0].high + "/" + result[0].forecast[0].low + ")";
+
+          bot.reply(message, botMessage);
+          console.log(new Date().toUTCString() + ": " + botMessage);
 
           for (var day = 1; day < result[0].forecast.length; day++) {
             if (result[0].forecast[day].precip >= 50) {
